6d5b485c81157473d0c39e5c1ae471f35a5b2be3
Moritz Ben Konschack <moritzkonschack@gmail.com>
2019-08-05 14:23:46 +1200

    Integrated index action for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,7 +20,7 @@ puts "5 skills created"
     subtitle: "My great service",
     body: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
     main_image: "https://place-hold.it/600x400.jpg&text=my%20picture"  ,
-    thumb_image: "https://place-hold.it/350x200.jpg&text=my%20picture" ,
+    thumb_image: "https://place-hold.it/400x200.jpg&text=my%20picture" ,
 
     )
 end
