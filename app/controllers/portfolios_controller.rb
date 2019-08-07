6906ed95d1037f7a9c7261452ea045d108c9912b
Moritz Ben Konschack <moritzkonschack@gmail.com>
2019-08-07 16:52:15 +1200

    Implemented show functioncality for portfolio feature

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,13 +4,13 @@ class PortfoliosController < ApplicationController
   end 
 
   def new
-    @portfolio_items = Portfolio.new
+    @portfolio_item = Portfolio.new
   end
 
   def create
-    @portfolio_items = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
      respond_to do |format|
-      if @portfolio_items.save
+      if @portfolio_item.save
         format.html { redirectto portfolios_path, notice: 'Blog was successfully created.' }
       else
         format.html { render :new }
@@ -19,19 +19,23 @@ class PortfoliosController < ApplicationController
   end
 
   def edit
-    @portfolio_items = Portfolio.find(params[:id])
+    @portfolio_item = Portfolio.find(params[:id])
   end
 
 
   def update
-    @portfolio_items = Portfolio.find(params[:id])
+    @portfolio_item = Portfolio.find(params[:id])
     respond_to do |format|
-      if @portfolio_items.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
         format.html { redirect_to portfolios_path, notice: 'Record was successfully updated.' }
       else
         format.html { render :edit }
       end
     end
   end
+
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
+  end 
 
 end
